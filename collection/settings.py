cea1c9a3e05b899e179cc1b37d2e64a1e234a3bf
reint <reintkamp@gmail.com>
2015-09-13 15:35:46 +0200

    leftovers from database switch

--- collection/settings.py
+++ collection/settings.py
@@ -62,8 +62,13 @@ WSGI_APPLICATION = 'collection.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'antiqueCollection',
+        'USER': 'cccplex',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
